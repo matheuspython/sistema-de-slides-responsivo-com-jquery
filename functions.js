f2d85a4b8d619a16be0081fa46ab1b4355b00043
matheus souza <ruivorobado@gmail.com>
2020-11-20 16:47:07 -0300

    adicionado dinamicamente as bolinhas do slider alem de animadas

--- functions.js
+++ functions.js
@@ -16,22 +16,47 @@ $(function() {
     }
 
     function buttons() {
+        for (var i = 0; i < total; i++) {
+            if (i == 0) {
+                $('.container-slider-buttons').append('<span style="background-color:black"></span>')
+            } else {
+                $('.container-slider-buttons').append('<span></span>')
+            }
+        }
+        $('.container-slider-buttons span').click(function() {
+            $('.container-slider-buttons span').css('background', '#ccc')
+            $(this).css('background', 'black')
+            $('.slider img').eq(indexAtual).stop().fadeOut(2000)
+            indexAtual = $(this).index()
+            $('.slider img').eq(indexAtual).stop().fadeIn(2000)
 
+        })
     }
 
     function mudaImg() {
+
+
+
         if (indexAtual > total) {
             indexAtual = 0
         }
+
+
+
         if (indexAtual == 0) {
+
             $('.slider img').eq(indexAtual).stop().fadeIn(2000)
-            $('.slider img').eq(indexAtual).stop().fadeOut(2000)
         } else {
             $('.slider img').eq(indexAtual).stop().fadeOut(2000)
         }
 
         indexAtual++
         $('.slider img').eq(indexAtual).stop().fadeIn(2000)
+
+        $('.container-slider-buttons span').css('background', '#ccc')
+
+        $('.container-slider-buttons span').eq(indexAtual).css('background', 'black')
+
     }
 
 
